331467dce03dc2396211fd086c0ef34963033cad
yym-yumeng123 <18026493873@163.com>
2021-10-10 22:59:03 +0800

    添加 Switch

--- src/views/App.js
+++ src/views/App.js
@@ -1,24 +1,17 @@
 import "./App.scss"
-import { Route, Redirect } from "react-router-dom"
+import { Route, Redirect, Switch } from "react-router-dom"
 import Login from "./login/login.jsx"
 import Register from "./register/rejister.jsx"
 
 function App() {
   return (
     <div className='App'>
-      {/* <ul>
-        <li>
-          <Link to='/login'>登录</Link>
-        </li>
-        <li>
-          <Link to='/register'>注册</Link>
-        </li>
-      </ul> */}
-
-      {/* 配置路由规则  exact表示精确匹配，防止匹配其他页面的时候匹配到/，也就是首页*/}
-      <Route path='/login' exact component={Login}></Route>
-      <Route path='/register' component={Register}></Route>
-      <Redirect to="/login" />
+      <Switch>
+        {/* 配置路由规则  exact表示精确匹配，防止匹配其他页面的时候匹配到/，也就是首页*/}
+        <Route path='/login' exact component={Login}></Route>
+        <Route path='/register' component={Register}></Route>
+        <Redirect to='/login' />
+      </Switch>
     </div>
   )
 }
